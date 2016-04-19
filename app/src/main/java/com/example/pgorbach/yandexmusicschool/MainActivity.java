7aa149db67f095616066382f6e1c1d8307c34018
Pavel Gorbach <rgnrok91@gmail.com>
2016-04-19 14:24:05 +0300

    some fixes

--- app/src/main/java/com/example/pgorbach/yandexmusicschool/MainActivity.java
+++ app/src/main/java/com/example/pgorbach/yandexmusicschool/MainActivity.java
@@ -121,9 +121,9 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_main, menu);
 
         final MenuItem refreshItem = menu.findItem(R.id.menu_refresh);
-
-        MenuItem searchViewItem = menu.findItem(R.id.action_search);
+        final MenuItem searchViewItem = menu.findItem(R.id.action_search);
         final SearchView searchViewAndroidActionBar = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+
         searchViewAndroidActionBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -138,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Disable pull-refresh if searching
         MenuItemCompat.setOnActionExpandListener(searchViewItem,
                 new MenuItemCompat.OnActionExpandListener() {
                     @Override
